b328cb727659e4b04ab9ed0f8629cf4403d79c42
Samantha Ogana <samanthaogana@gmail.com>
2024-10-02 12:48:46 +0300

    Correct code

--- quiz.js
+++ quiz.js
@@ -6,10 +6,9 @@ function checkAnswer() {
     let correctAnswer= "4";
     let userAnswer= document.querySelector('input[name="quiz"]:checked').value;
 
-    if(userAnswer === correctAnswer) {
+    if (userAnswer === correctAnswer) {
         document.getElementById('feedback').textContent ="Correct! Well done";
-    }
-    else{
+    } else{
         document.getElementById('feedback').textContent ="That's incorrect. Try again!";
     }
 }
