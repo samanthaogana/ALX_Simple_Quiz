4b8b0603d29cd2223bdd457b72cda19b96636261
Samantha Ogana <samanthaogana@gmail.com>
2024-10-02 12:45:02 +0300

    Modify quiz.js

---
+++ quiz.js
@@ -0,0 +1,17 @@
+function checkAnswer() {
+    // Function body
+}
+
+function checkAnswer() {
+    let correctAnswer= "4";
+    let userAnswer= document.querySelector('input[name="quiz"]:checked').value;
+
+    if(userAnswer === correctAnswer) {
+        document.getElementById('feedback').textContent ="Correct! Well done";
+    }
+    else{
+        document.getElementById('feedback').textContent ="That's incorrect. Try again!";
+    }
+}
+
+document.getElementById('usbmit-answer').addEventListener('click', checkAnswer);
